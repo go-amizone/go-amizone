7882fb8ccf1136c7398dbb1832ac66c8ca747eda
ditsuke <ditsuke@protonmail.com>
2022-04-30 14:02:34 +0530

    feat: Implement response-based auth status parser
    This internal parser tries to ascertain authentication status
    based on response bodies. We'll eventually use this to make the client
    more robust once we refactor the request-parse model to probably factor
    out colly for pure net/http + goquery and accommodate expired
    authentication better through login-retries.

---
+++ lib/amizone/internal/parse/logged_in.go
@@ -0,0 +1,26 @@
+package parse
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+)
+
+const loginFormHtmlId = "loginform"
+
+// LoggedIn attempts to determine whether a response body indicates an authenticated session.
+// To achieve this, this function will first attempt to parse the body as an HTML document, failing to do
+// which is assumed to indicate an authenticated session because Amizone seems to redirect unauthenticated requests
+// from all endpoints to the login page.
+// If the body is parsed into a HTMl document, this function will attempt to find the login form; failing to find
+// the login form is assumed to indicate an authenticated session.
+func LoggedIn(body []byte) bool {
+	// Try to find the login form
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
+	if err != nil { // Failure to parse an HTML document ~ logged-in
+		return true
+	}
+
+	form := doc.Find(fmt.Sprintf("#%s", loginFormHtmlId)).First()
+	return form.Length() == 0
+}
