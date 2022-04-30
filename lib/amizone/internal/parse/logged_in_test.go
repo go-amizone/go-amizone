29dabb1214cc2a2b8036e12843cc3b4aab9fbedb
ditsuke <ditsuke@protonmail.com>
2022-04-30 14:03:30 +0530

    test: Add unit tests for login status parser

---
+++ lib/amizone/internal/parse/logged_in_test.go
@@ -0,0 +1,49 @@
+package parse_test
+
+import (
+	"GoFriday/lib/amizone/internal/parse"
+	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"testing"
+)
+
+const (
+	LoginPageFile        = "../../testdata/login_page.html"
+	LoggedInHomePageFile = "../../testdata/home_page_logged_in.html"
+	ScheduleJsonNonEmpty = "../../testdata/diary_events.json"
+)
+
+func TestLoggedIn(t *testing.T) {
+	testcases := []struct {
+		name     string
+		bodyFile string
+		expected bool
+	}{
+		{
+			name:     "logged in",
+			bodyFile: LoggedInHomePageFile,
+			expected: true,
+		},
+		{
+			name:     "not logged in",
+			bodyFile: LoginPageFile,
+			expected: false,
+		},
+		{
+			name:     "json schedule",
+			bodyFile: ScheduleJsonNonEmpty,
+			expected: true,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			g := NewGomegaWithT(t)
+
+			fileContents, err := ioutil.ReadFile(tc.bodyFile)
+			g.Expect(err).ToNot(HaveOccurred())
+
+			g.Expect(parse.LoggedIn(fileContents)).To(Equal(tc.expected))
+		})
+	}
+}
