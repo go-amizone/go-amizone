3da2eb20e05e503969fa1440b20670a5305893c1
ditsuke <ditsuke@protonmail.com>
2022-04-30 16:40:48 +0530

    refactor: Refactor amizoneClient.login() with pure goquery
    - Linear control flow
    - (hopefully) increased readability
    - More robust login validation with the new parse.LoggedIn fn
    - New amizoneClient.DidLogin() method to answer: "Did we ever log in?"
      This method can also provide clarity when empty credentials are
      passed, resulting in no login attempt and no error.

--- lib/amizone/interface.go
+++ lib/amizone/interface.go
@@ -2,6 +2,7 @@ package amizone
 
 // ClientInterface is an exported interface for amizoneClient to make mocking and testing more convenient.
 type ClientInterface interface {
+	DidLogin() bool
 	GetAttendance() (AttendanceRecord, error)
 	GetClassSchedule(date Date) (classSchedule, error)
 }
