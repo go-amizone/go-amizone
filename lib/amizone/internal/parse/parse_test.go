cfea7ade8fa9c9bfe3f2b88999c54ce89d0c3a24
ditsuke <ditsuke@protonmail.com>
2022-04-30 17:32:57 +0530

    tests(unit): Add tests for the verification token parser
    Also does a minor refactor for the parse_test package

---
+++ lib/amizone/internal/parse/parse_test.go
@@ -0,0 +1,8 @@
+package parse_test
+
+// Constants used across the tests
+const (
+	LoginPageFile        = "../../testdata/login_page.html"
+	LoggedInHomePageFile = "../../testdata/home_page_logged_in.html"
+	ScheduleJsonNonEmpty = "../../testdata/diary_events.json"
+)
---
+++ lib/amizone/internal/parse/verification_token_test.go
@@ -0,0 +1,39 @@
+package parse_test
+
+import (
+	"GoFriday/lib/amizone/internal/parse"
+	. "github.com/onsi/gomega"
+	"os"
+	"testing"
+)
+
+func TestVerificationToken(t *testing.T) {
+	testCases := []struct {
+		name          string
+		bodyFile      string
+		expectedToken string
+	}{
+		{
+			name:          "login page, verification token exists",
+			bodyFile:      LoginPageFile,
+			expectedToken: "LV571ePb0TV-evRywWVGfbpe5PE71EpyM2U_9MGu69GA8-tlD4TaVd265sXZPoPyA2Xh2qV7D2t-8yKJWYzK17wyEMKuPseFtRk25WAqeC81",
+		},
+		{
+			name:          "home page, verification token does not exist",
+			bodyFile:      LoggedInHomePageFile,
+			expectedToken: "",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			g := NewGomegaWithT(t)
+
+			body, err := os.Open(tc.bodyFile)
+			g.Expect(err).ToNot(HaveOccurred())
+
+			token := parse.VerificationToken(body)
+			g.Expect(token).To(Equal(tc.expectedToken))
+		})
+	}
+}
