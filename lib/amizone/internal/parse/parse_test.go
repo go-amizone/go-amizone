ee13fefee98e0e194fcd2ebbae13f3f14843b368
ditsuke <ditsuke@protonmail.com>
2022-05-01 16:24:03 +0530

    test(unit): Add test cases for the exam schedule parser

--- lib/amizone/internal/parse/parse_test.go
+++ lib/amizone/internal/parse/parse_test.go
@@ -2,7 +2,8 @@ package parse_test
 
 // Constants used across the tests
 const (
-	LoginPageFile        = "../../testdata/login_page.html"
-	LoggedInHomePageFile = "../../testdata/home_page_logged_in.html"
-	ScheduleJsonNonEmpty = "../../testdata/diary_events.json"
+	LoginPageFile           = "../../testdata/login_page.html"
+	LoggedInHomePageFile    = "../../testdata/home_page_logged_in.html"
+	ScheduleJsonNonEmpty    = "../../testdata/diary_events.json"
+	ExaminationSchedulePage = "../../testdata/examination_schedule.html"
 )
