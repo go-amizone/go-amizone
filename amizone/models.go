76a8f4219518a1b3db2fdeb74538da70779adeaf
ditsuke <ditsuke@protonmail.com>
2022-05-04 16:02:33 +0530

    feat(sdk): Export data models

---
+++ amizone/models.go
@@ -0,0 +1,12 @@
+package amizone
+
+import "amizone/amizone/internal/models"
+
+// Attendance is a model for representing attendance from the portal.
+type Attendance models.AttendanceRecord
+
+// ClassSchedule is a model for representing class schedule from the portal.
+type ClassSchedule models.ClassSchedule
+
+// ExamSchedule is a model for representing exam schedule from the portal.
+type ExamSchedule models.ExaminationSchedule
