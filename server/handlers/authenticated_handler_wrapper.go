b49928f95ddc0f612c541146f37c57ba1379c69a
ditsuke <ditsuke@protonmail.com>
2022-05-05 19:31:42 +0530

    refactor(api-server): Decouple server and command
    With this commit, the server and the command are decoupled. The command
    now rests in the conventional /cmd/<command-name> location. The new
    server architecture opens up the possibility of better testing, too,
    with the use of the `amizone.ClientFactoryInterface` for the
    authentication handler layer.

--- server/handlers/authenticated_handler_wrapper.go
+++ server/handlers/authenticated_handler_wrapper.go
@@ -5,19 +5,11 @@ import (
 	"net/http"
 )
 
-// AuthenticatedHandler functions handle requests to Amizone that require auth.
-// They need to be wrapped by a decorator that checks the auth parameters and creates an amizone.ClientInterface
-// instance before calling onto the authenticated handler.
-type AuthenticatedHandler func(
-	rw http.ResponseWriter,
-	r *http.Request,
-	c amizone.ClientInterface)
-
 // authenticatedHandlerWrapper wraps an AuthenticatedHandler, composing a http.HandlerFunc
 // This function handles retrieving authentication information from the request, initializing
 // an amizone.amizoneClient with the information, and then passing this to the handler.
 // This function also handles authentication errors if the auth information is invalid.
-func authenticatedHandlerWrapper(a *handlerCfg, handler AuthenticatedHandler) http.HandlerFunc {
+func authenticatedHandlerWrapper(c *Cfg, handler AuthenticatedHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Get query parameters for auth
 		username := r.FormValue("username")
@@ -28,7 +20,7 @@ func authenticatedHandlerWrapper(a *handlerCfg, handler AuthenticatedHandler) ht
 			return
 		}
 
-		client, err := amizone.NewClient(
+		client, err := c.A(
 			amizone.Credentials{
 				Username: username,
 				Password: password,
@@ -40,7 +32,7 @@ func authenticatedHandlerWrapper(a *handlerCfg, handler AuthenticatedHandler) ht
 				w.WriteHeader(http.StatusUnauthorized)
 				return
 			}
-			a.l.Error(err, "error creating amizone client")
+			c.L.Error(err, "error creating amizone client")
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
---
+++ server/server.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"context"
+	"github.com/ditsuke/go-amizone/amizone"
+	"github.com/ditsuke/go-amizone/server/handlers"
+	"github.com/go-logr/logr"
+	"net/http"
+)
+
+type AmizoneClientFactory func(amizone.Credentials) amizone.ClientInterface
+
+type Config struct {
+	Logger   logr.Logger
+	BindAddr string
+}
+
+func NewConfig() *Config {
+	return &Config{
+		BindAddr: "127.0.0.1:8081",
+	}
+}
+
+type ApiServer struct {
+	Config     *Config
+	Router     *http.ServeMux
+	httpServer *http.Server
+}
+
+func New(config *Config) *ApiServer {
+	return &ApiServer{
+		Config: config,
+		Router: http.NewServeMux(),
+	}
+}
+
+func (s *ApiServer) Run() error {
+	if s.httpServer == nil {
+		s.Config.Logger.V(1).Info("Initializing server...")
+		s.httpServer = &http.Server{
+			Addr:    s.Config.BindAddr,
+			Handler: s.Router,
+		}
+	}
+
+	s.Config.Logger.Info("Starting server", "bind_addr", s.Config.BindAddr)
+	s.configureRouter()
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *ApiServer) Stop(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
+
+func (s *ApiServer) configureRouter() {
+	handlerCfg := handlers.NewCfg(s.Config.Logger, func(cred amizone.Credentials, httpClient *http.Client) (amizone.ClientInterface, error) {
+		return amizone.NewClient(cred, httpClient)
+	})
+
+	s.Router.HandleFunc("/attendance", handlerCfg.AttendanceHandler)
+	s.Router.HandleFunc("/class_schedule", handlerCfg.ClassScheduleHandler)
+	s.Router.HandleFunc("/exam_schedule", handlerCfg.ExamScheduleHandler)
+}
