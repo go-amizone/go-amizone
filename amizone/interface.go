ed87958c2a444445e1fb48467d04154034db3e9a
ditsuke <ditsuke@protonmail.com>
2022-05-05 19:05:19 +0530

    feat(sdk): Add client manufacturer interface
    - This interface makes it easier to decouple consumers of the client
      from the concrete amizoneClient interface, making it easier to
      test through DI without consumers having to define their own
      types.
    - Interface compliance constraints are moved to the interface file true
      to Go's spirit of implementors not coupled with interface definitions.

--- amizone/interface.go
+++ amizone/interface.go
@@ -2,6 +2,7 @@ package amizone
 
 import (
 	"github.com/ditsuke/go-amizone/amizone/internal/models"
+	"net/http"
 )
 
 // ClientInterface is an exported interface for amizoneClient to make mocking and testing more convenient.
@@ -10,4 +11,18 @@ type ClientInterface interface {
 	GetAttendance() (models.AttendanceRecord, error)
 	GetClassSchedule(date Date) (models.ClassSchedule, error)
 	GetExamSchedule() (*models.ExaminationSchedule, error)
+}
+
+// Interface compliance constraint for amizoneClient
+var _ ClientInterface = &amizoneClient{}
+
+// ClientFactoryInterface is a type for functions that return ClientInterface
+// instances. Functions returning concrete types need to be wrapped by functions
+// that return the interface; apparently a limitation of the Go compiler's type
+// inference.
+type ClientFactoryInterface func(cred Credentials, httpClient *http.Client) (ClientInterface, error)
+
+// Interface compliance constraint for NewClient [requires wrapping]
+var _ ClientFactoryInterface = func(cred Credentials, httpClient *http.Client) (ClientInterface, error) {
+	return NewClient(cred, httpClient)
 }
