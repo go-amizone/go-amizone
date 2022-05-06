7d59a3efb361f90b07cd62febd2f7f9945bf4468
ditsuke <ditsuke@protonmail.com>
2022-05-06 20:18:37 +0530

    feat(api): Support config through flags
    - The server API now supports configuration through command line flags.
    - Environment variable remain, but they're overridden by flags.
    - Absence of .env file in the wd is no longer logged to stderr.

--- cmd/amizone-api-server/server.go
+++ cmd/amizone-api-server/server.go
@@ -2,35 +2,46 @@ package main
 
 import (
 	"context"
+	"flag"
 	"github.com/ditsuke/go-amizone/server"
 	"github.com/joho/godotenv"
 	"k8s.io/klog/v2"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"time"
+)
+
+const (
+	DefaultAddress = ":8081"
+	AddressEnvVar  = "AMIZONE_API_ADDRESS"
 )
 
 func main() {
 	logger := klog.NewKlogr()
+	_ = godotenv.Load(".env")
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		logger.Error(err, "Failed to load .env file")
+	config := struct{ address string }{}
+
+	flagSet := flag.NewFlagSet("server config", flag.ExitOnError)
+	flagSet.StringVar(&config.address, "address", EnvOrDefault(AddressEnvVar, DefaultAddress), "Address to listen on")
+	if err := flagSet.Parse(os.Args[1:]); err != nil {
+		logger.Error(err, "failed to parse flags")
+		os.Exit(1)
 	}
-	address := os.Getenv("AMIZONE_API_ADDRESS")
 
 	s := server.ApiServer{
 		Config: &server.Config{
 			Logger:   logger,
-			BindAddr: address,
+			BindAddr: config.address,
 		},
 		Router: http.NewServeMux(),
 	}
 
 	// Start the server on a new go-thread
 	go func() {
-		logger.Info("Starting server", "address", address)
+		logger.Info("Starting server", "address", config.address)
 		if err := s.Run(); err != nil && err != http.ErrServerClosed {
 			panic(err)
 		}
@@ -50,10 +61,28 @@ func main() {
 	ctx, cancelFunc := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancelFunc()
 
-	err = s.Stop(ctx)
-	if err != nil {
+	if err := s.Stop(ctx); err != nil {
 		logger.Error(err, "failed to gracefully shut down serer", err)
 	}
 
 	logger.Info("server gracefully shut down")
+}
+
+// EnvOrDefault is a generic implementation that returns either the environment variable accessed by `key`
+// or the default value.
+func EnvOrDefault[T string | int | bool](key string, def T) T {
+	env, ok := os.LookupEnv(key)
+	if !ok {
+		return def
+	}
+	var ret T
+	switch p := any(&ret).(type) {
+	case *string:
+		*p = env
+	case *int:
+		*p, _ = strconv.Atoi(env)
+	case *bool:
+		*p, _ = strconv.ParseBool(env)
+	}
+	return ret
 }
