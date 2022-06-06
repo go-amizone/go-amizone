1e239efe4a5ea62a37cec57de5b07b4bd42a4915
ditsuke <ditsuke@protonmail.com>
2022-06-06 14:08:09 +0530

    fix: Update assignment in schedule parser
    Missed updating this post the refactoring some commits back.

--- amizone/internal/parse/examination_schedule.go
+++ amizone/internal/parse/examination_schedule.go
@@ -64,7 +64,7 @@ func ExaminationSchedule(body io.Reader) (*models.ExaminationSchedule, error) {
 	// Iterate through exams rows to parse entries
 	scheduleEntries.Each(func(i int, row *goquery.Selection) {
 		exam := models.ScheduledExam{
-			Course: &models.Course{
+			Course: &models.CourseRef{
 				Code: row.Find(fmt.Sprintf(dataCellSelectorTpl, dTitleCode)).Text(),
 				Name: row.Find(fmt.Sprintf(dataCellSelectorTpl, dTitleName)).Text(),
 			},
