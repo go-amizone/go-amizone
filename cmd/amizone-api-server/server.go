0631872793c13e2123bff6f39acac5a0033205cb
ditsuke <ditsuke@protonmail.com>
2022-06-26 15:12:49 +0530

    first kin of functional grpc + http impl

--- cmd/amizone-api-server/server.go
+++ cmd/amizone-api-server/server.go
@@ -4,22 +4,32 @@ import (
 	"context"
 	"flag"
 	"github.com/ditsuke/go-amizone/server"
+	v1 "github.com/ditsuke/go-amizone/server/gen/go/v1"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/joho/godotenv"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
 	"k8s.io/klog/v2"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"time"
 )
 
 const (
-	DefaultAddress = ":8081"
+	DefaultAddress = "0.0.0.0:8081"
 	AddressEnvVar  = "AMIZONE_API_ADDRESS"
 )
 
 func main() {
 	logger := klog.NewKlogr()
+	ctxBg := context.Background()
 	_ = godotenv.Load(".env")
 
 	config := struct{ address string }{}
@@ -31,18 +41,38 @@ func main() {
 		os.Exit(1)
 	}
 
-	s := server.ApiServer{
-		Config: &server.Config{
-			Logger:   logger,
-			BindAddr: config.address,
-		},
-		Router: http.NewServeMux(),
+	grpcMux := grpc.NewServer()
+	v1.RegisterAmizoneServiceServer(grpcMux, server.NewAmizoneServiceServer([]byte("nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn")))
+	reflection.Register(grpcMux)
+
+	gwMux := runtime.NewServeMux()
+	err := v1.RegisterAmizoneServiceHandlerFromEndpoint(ctxBg, gwMux, "localhost:8081", []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())})
+	if err != nil {
+		logger.Error(err, "failed to register grpc gateway")
+		os.Exit(1)
+	}
+
+	// Get a tcp network listener
+	conn, err := net.Listen("tcp", config.address)
+	if err != nil {
+		panic(err)
+	}
+
+	s := &http.Server{
+		Addr: config.address,
+		Handler: h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if isGrpc(r) { // Forward to the gRPC server
+				grpcMux.ServeHTTP(w, r)
+				return
+			}
+			gwMux.ServeHTTP(w, r)
+		}), &http2.Server{}),
 	}
 
 	// Start the server on a new go-thread
 	go func() {
 		logger.Info("Starting server", "address", config.address)
-		if err := s.Run(); err != nil && err != http.ErrServerClosed {
+		if err := s.Serve(conn); err != nil && err != http.ErrServerClosed {
 			panic(err)
 		}
 	}()
@@ -58,10 +88,10 @@ func main() {
 	// Log the signal
 	logger.Info("os signal received", "signal", sig)
 
-	ctx, cancelFunc := context.WithTimeout(context.Background(), 20*time.Second)
+	cancelCtx, cancelFunc := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancelFunc()
 
-	if err := s.Stop(ctx); err != nil {
+	if err := s.Shutdown(cancelCtx); err != nil {
 		logger.Error(err, "failed to gracefully shut down serer", err)
 	}
 
@@ -85,4 +115,14 @@ func EnvOrDefault[T string | int | bool](key string, def T) T {
 		*p, _ = strconv.ParseBool(env)
 	}
 	return ret
+}
+
+func isGrpc(r *http.Request) bool {
+	//if r.ProtoAtLeast(2, 0) && strings.HasPrefix(r.Header.Get("Content-Type"), "application/grpc") {
+	//	return true
+	//}
+	if r.ProtoMajor == 2 && strings.HasPrefix(r.Header.Get("Content-Type"), "application/grpc") {
+		return true
+	}
+	return false
 }
