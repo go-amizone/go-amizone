b522725085ae0df7c13da8c5b2a126e9906b8598
ditsuke <ditsuke@protonmail.com>
2023-04-16 20:23:22 +0530

    chore: switch to binary installation for golangci-lint

--- hack/tools/tools.go
+++ hack/tools/tools.go
@@ -4,7 +4,6 @@
 package tools
 
 import (
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
 	_ "github.com/rakyll/gotest"
