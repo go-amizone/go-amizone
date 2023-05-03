10ecd7bff480fb8173e593d467998c87606e6305
Achintya <achintya22052000@gmail.com>
2023-05-03 20:14:35 +0530

    modified payload struct

--- token/payload.go
+++ token/payload.go
@@ -1,14 +1,12 @@
 package token
 
 type Payload struct {
-	UserID   string `json:"user_id"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
-func NewPayload(userID string, email string, password string) *Payload {
+func NewPayload(email string, password string) *Payload {
 	return &Payload{
-		UserID:   userID,
 		Email:    email,
 		Password: password,
 	}
